7a10d7f085b0d9d3ce910ffac71518fa696fe65e
Aditya Singh <singh.adi7048@gmail.com>
2025-09-01 22:33:15 +0530

    Time: 1 ms (37.16%) | Memory: 28.2 MB (28.28%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -10,9 +10,10 @@ public:
             }
             else{
                 freq=1;
-                ans=nums[i];
+               
             }
             if(freq>n/2){
+                ans=nums[i-1];
                 return ans;
             }
         }
