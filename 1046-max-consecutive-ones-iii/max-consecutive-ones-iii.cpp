f6907b986b4e856fb7cdd2e3355b5593c69dd405
Aditya Singh <singh.adi7048@gmail.com>
2026-02-23 17:13:25 +0530

    Time: 0 ms (100.00%) | Memory: 69.5 MB (91.58%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.cpp
@@ -7,7 +7,7 @@ public:
             if(nums[r]==0){
                 count++;
             }
-            while(count>k){
+            if(count>k){
                 if(nums[l]==0)count--;
                 l++;
             }
